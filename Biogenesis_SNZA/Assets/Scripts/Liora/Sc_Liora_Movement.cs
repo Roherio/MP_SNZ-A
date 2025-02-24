906313812d4d3f896a36b1b54e6df121a5b9c5d0
Ferali7 <aliagarodriguezferran@gmail.com>
2025-02-24 22:06:44 +0100

    arregloMovimiento

--- Biogenesis_SNZA/Assets/Scripts/Liora/Sc_Liora_Movement.cs
+++ Biogenesis_SNZA/Assets/Scripts/Liora/Sc_Liora_Movement.cs
@@ -6,14 +6,16 @@ using UnityEngine.InputSystem;
 public class Sc_Liora_Movement : MonoBehaviour
 {
     Rigidbody2D rb;
-    Animator animator;
-    [SerializeField] float groundSpeed = 5f;
-    [SerializeField] float jumpPower = 10f;
     public BoxCollider2D groundCheck;
     public LayerMask groundLayer;
-    [SerializeField] private bool isGrounded;
-    private bool jumping;
-    Vector2 movim;
+
+    Animator animator;
+    private float horizontal;
+    [SerializeField] float groundSpeed = 5f;
+    [SerializeField] float jumpPower = 10f;
+    private bool isFacingRight = true;
+
+    public bool jumping;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,21 +26,27 @@ public class Sc_Liora_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(movim.x * groundSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(horizontal * groundSpeed, rb.velocity.y);
+        if (!isFacingRight && horizontal > 0f)
+        {
+            FlipSprite();
+        }
+        else if (isFacingRight && horizontal < 0f)
+        {
+            FlipSprite();
+        }
         StateMachine();
-        FlipSprite();
         //fa check si el objecte empty groundCheck fa overlap amb un objecte que pertany a la groundLayer (overlap amb radi de 0.2f)
-        CheckGround();
     }
 
     public void Movimiento(InputAction.CallbackContext context)
     {
         //print("moviendosee");
-        movim = context.ReadValue<Vector2>();
+        horizontal = context.ReadValue<Vector2>().x;
     }
     public void Saltar(InputAction.CallbackContext context)
     {
-        if (context.started && isGrounded)
+        if (context.started && CheckGround())
         {
             //saltarà amb la primera input (context.started, sinó seria input continu) nomes si isGrounded es vertader
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
@@ -48,7 +56,8 @@ public class Sc_Liora_Movement : MonoBehaviour
         if (context.canceled && rb.velocity.y > 0)
         {
             //si deixes de premer el botó (context.canceled) i està pujant encara (rb.velocity.y > 0), tallarà el salt fent que baixi la seva velocitat vertical a la meitat
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            //rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            rb.AddForce(new Vector2(0, -4f), ForceMode2D.Impulse);
         }
     }
     public void StateMachine()
@@ -56,32 +65,36 @@ public class Sc_Liora_Movement : MonoBehaviour
         animator.SetBool("isRunning", false);
         animator.SetBool("isJumping", false);
         animator.SetBool("isFalling", false);
-        if (isGrounded && rb.velocity.x != 0)
+        if (CheckGround() && rb.velocity.x != 0)
         {
             animator.SetBool("isRunning", true);
         }
-        if (isGrounded && jumping)
+        if (CheckGround() && jumping)
         {
             animator.SetBool("isJumping", true);
         }
-        if (!isGrounded && rb.velocity.y < 0)
+        if (!CheckGround() && rb.velocity.y < 0)
         {
             animator.SetBool("isFalling", true);
         }
     }
-    public void FlipSprite()
+    private void FlipSprite()
     {
-        if (rb.velocity.x < 0)
+        isFacingRight = !isFacingRight;
+        Vector2 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+        /*if (rb.velocity.x < 0)
         {
             transform.localScale = new Vector2(-1, 1);
         }
         if (rb.velocity.x > 0)
         {
             transform.localScale = new Vector2(1, 1);
-        }
+        }*/
     }
-    public void CheckGround()
+    private bool CheckGround()
     {
-        isGrounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundLayer).Length > 0;
+        return Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundLayer).Length > 0;
     }
 }
