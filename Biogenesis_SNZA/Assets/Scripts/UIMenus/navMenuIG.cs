a680c24cd898216227034bf15146206930ed77ec
Yuri Stoilkov Sokolov <yuri.s.s.rb@gmail.com>
2025-03-13 13:41:10 +0100

    Mejoras menu in game

--- Biogenesis_SNZA/Assets/Scripts/UIMenus/navMenuIG.cs
+++ Biogenesis_SNZA/Assets/Scripts/UIMenus/navMenuIG.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class navMenuIG : MonoBehaviour
 {
 
     public GameObject[] ArrTxtMenuIG = new GameObject[6];
+    public Button[] ArrBotones = new Button[6];
     public static int idBotonActivo = 0;
+    public Vector3 botonGrande = new Vector3(1.1f, 1.1f, 1);
+    private Vector3 botonNormal = new Vector3(1, 1, 1);
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +21,7 @@ public class navMenuIG : MonoBehaviour
         }
         idBotonActivo = scrMemoria.memIdBotonActivo;
         ArrTxtMenuIG[idBotonActivo].SetActive(true);
+        BotonPulsado(idBotonActivo);
     }
 
     // Update is called once per frame
@@ -24,11 +29,17 @@ public class navMenuIG : MonoBehaviour
     {
         
     }
+    public void ResetBotones()
+    {
+        
+    }
     public void BotonPulsado(int idBotonPulsado)
     {
         ArrTxtMenuIG[idBotonActivo].SetActive(false);
+        ArrBotones[idBotonActivo].transform.localScale = botonNormal;
         idBotonActivo = idBotonPulsado;
         scrMemoria.GuardarIdBotonActivo();
         ArrTxtMenuIG[idBotonActivo].SetActive(true);
+        ArrBotones[idBotonActivo].transform.localScale = botonGrande;
     }
 }
