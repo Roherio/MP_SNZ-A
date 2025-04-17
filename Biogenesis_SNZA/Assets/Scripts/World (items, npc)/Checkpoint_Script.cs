d308855deb4cba7dcf347cc5c9f994696fcc46bb
Ferali7 <aliagarodriguezferran@gmail.com>
2025-04-17 13:42:01 +0200

    push palanca prefabs

---
+++ Biogenesis_SNZA/Assets/Scripts/World (items, npc)/Checkpoint_Script.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint_Script : MonoBehaviour, IInteractable_Script
+{
+    public bool CanInteract()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
