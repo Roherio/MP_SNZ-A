a01211a8a0ccb290d1194d9de02b98da8eb2e910
Ferali7 <aliagarodriguezferran@gmail.com>
2025-05-06 19:39:47 +0200

    attackFix

--- Biogenesis_SNZA/Assets/Scripts/Liora/Liora_Attack_Script.cs
+++ Biogenesis_SNZA/Assets/Scripts/Liora/Liora_Attack_Script.cs
@@ -7,43 +7,41 @@ using UnityEngine.InputSystem;
 public class Liora_Attack_Script : MonoBehaviour
 {
     public Animator animator;
+    //---------------------------------------ATAQUE LOGIC
     public enum snzaAttackType { NONE, CANGREJO, ESCARABAJO, SECRETARIO, AGUILA, JABALI }
     [SerializeField] public static snzaAttackType currentAttackType = snzaAttackType.CANGREJO;
     public static bool isAttacking = false;
-    public float inputAttackCooldown = 0.4f;
+    public float inputAttackCooldown = 0.2f;
     private float inputCooldownTimer;
-
     //combo Logic
     public static int currentComboStep = 0;
     private bool canReceiveNextComboInput = true;
     //private bool bufferedNextComboInput = false; //variable que permet pulsar el següent atac encara que no haguem acabat el que s'esta fent
     private float comboTimer = 0f;
-    private float comboMaxTime = 0.8f;
+    private float comboMaxTime = 1f;
     public bool isComboActive = false;
-
-    public enum snzaParryType { NONE, CANGREJO, ESCARABAJO, SECRETARIO, AGUILA, JABALI }
-    [SerializeField] public static snzaParryType currentParryType = snzaParryType.CANGREJO;
-    public static bool isParrying = false;
-    private float parryCooldown = 2f;
-   
-    public enum snzaUltiType { NONE, MANTIS }
-    [SerializeField] public snzaUltiType currentUltiType;
-    public static bool isDoingUlti = false;
-
-    //----Colliders attack Liora
+    //posició on s'instanciaran els attacks Liora
     public Transform attackLocation;
-    public float duracioCollider;
-    public float delayCollider;
     //collider general
     private GameObject colliderAtaque;
     //colliders particulars
     public GameObject colliderAttackCrabLiora;
     public GameObject colliderAttackBoarLiora;
+    //---------------------------------------PARRY LOGIC
+    public enum snzaParryType { NONE, CANGREJO, ESCARABAJO, SECRETARIO, AGUILA, JABALI }
+    [SerializeField] public static snzaParryType currentParryType = snzaParryType.CANGREJO;
+    public static bool isParrying = false;
+    private float parryCooldown = 2f;
+    //---------------------------------------ULTI LOGIC
+    public enum snzaUltiType { NONE, MANTIS }
+    [SerializeField] public snzaUltiType currentUltiType;
+    public static bool isDoingUlti = false;
 
-    //variable que determinarà quin mal fa Liora amb aquell attack
-    public static float damageAttackLiora;
-    //variable per saber quan acaba l'estat isAttacking/parrying/doingUlti per cada moviment
-    public float deactivateAction;
+    //---------------------------------------VARIABLES PER CADA ATAC
+    public static float damageAttackLiora; //variable que determinarà quin mal fa Liora amb aquell attack
+    public float deactivateAction; //variable per saber quan acaba l'estat isAttacking/isParrying/isDoingUlti per cada moviment
+    public float duracioCollider; //determina quant temps està el collider de l'atac instanciat
+    public float delayCollider; //determina quin delay té per instanciar-se
 
     // Start is called before the first frame update
     void Start()
@@ -91,6 +89,7 @@ public class Liora_Attack_Script : MonoBehaviour
         Liora_StateMachine_Script.isParrying = isParrying;
         Liora_StateMachine_Script.isDoingUlti = isDoingUlti;
     }
+    //--------------------------------------------ATAQUE
     public void Ataque(InputAction.CallbackContext context)
     {
         //no entrarem a fer l'atac si el cooldownTimer segueix sent mes petit que el cooldown de l'atac
@@ -115,31 +114,6 @@ public class Liora_Attack_Script : MonoBehaviour
             }
         }
     }
-    public void Parry(InputAction.CallbackContext context)
-    {
-        if (GameControl_Script.isPaused) { return; }
-        //no entrarem a fer l'atac si el cooldownTimer segueix sent mes petit que el cooldown de l'atac
-        if (Liora_Movement_Script.isGrabbingLedge || inputAttackCooldown > inputCooldownTimer || isAttacking || isParrying || isDoingUlti) { return; }
-        if (parryCooldown > 0f) { return; }
-        if (context.started)
-        {
-            switch (currentParryType)
-            {
-                case snzaParryType.CANGREJO:
-                    //aqui determinem el temps que trigarà despres en acabarse l'animació d'attack, i també ressetejem el cooldownTimer perquè no pugui spammejar el atac
-                    deactivateAction = 0.8f;
-                    break;
-
-                case snzaParryType.JABALI:
-                    deactivateAction = 0.5f;
-                    break;
-            }
-            inputCooldownTimer = 0f;
-            isParrying = true;
-            parryCooldown = 2f;
-            StartCoroutine(DeactivateAction());
-        }
-    }
     private void HandleAttackStep(int step)
     {
         switch (currentAttackType)
@@ -150,27 +124,26 @@ public class Liora_Attack_Script : MonoBehaviour
                     case 1:
                         damageAttackLiora = 20f;
                         duracioCollider = 0.2f;
-                        delayCollider = 0.2f;
-                        deactivateAction = 0.4f;
+                        delayCollider = 0.1f;
+                        deactivateAction = 0.3f;
                         break;
                     case 2:
                         damageAttackLiora = 30f;
                         duracioCollider = 0.2f;
-                        delayCollider = 0.2f;
-                        deactivateAction = 0.4f;
+                        delayCollider = 0.1f;
+                        deactivateAction = 0.7f;
                         break;
                     case 3:
                         damageAttackLiora = 50f;
                         duracioCollider = 0.3f;
-                        delayCollider = 0.6f;
-                        deactivateAction = 1.5f;
+                        delayCollider = 0.2f;
+                        deactivateAction = 0.8f;
                         break;
                 }
                 colliderAtaque = colliderAttackCrabLiora;
                 Invoke("CallInstanciarAtaque", delayCollider);
                 //InstanciarAtaque(colliderAttackCrabLiora);
                 break;
-
             case snzaAttackType.JABALI:
                 damageAttackLiora = 50f;
                 deactivateAction = 0.5f;
@@ -182,12 +155,39 @@ public class Liora_Attack_Script : MonoBehaviour
     }
     void CallInstanciarAtaque()
     {
+        //funcio intermitja a la que fem INVOKE donantli el collider que volem utilitzar
         InstanciarAtaque(colliderAtaque);
     }
     void InstanciarAtaque(GameObject collider)
     {
         Instantiate(collider, attackLocation);
         AttackCollider_Script.duracioCollider = duracioCollider;
+    }
+    //--------------------------------------------PARRY
+    public void Parry(InputAction.CallbackContext context)
+    {
+        if (GameControl_Script.isPaused) { return; }
+        //no entrarem a fer l'atac si el cooldownTimer segueix sent mes petit que el cooldown de l'atac
+        if (Liora_Movement_Script.isGrabbingLedge || inputAttackCooldown > inputCooldownTimer || isAttacking || isParrying || isDoingUlti) { return; }
+        if (parryCooldown > 0f) { return; }
+        if (context.started)
+        {
+            switch (currentParryType)
+            {
+                case snzaParryType.CANGREJO:
+                    //aqui determinem el temps que trigarà despres en acabarse l'animació de parry, i també ressetejem el cooldownTimer perquè no pugui spammejar el parry
+                    deactivateAction = 1f;
+                    break;
+
+                case snzaParryType.JABALI:
+                    deactivateAction = 0.5f;
+                    break;
+            }
+            inputCooldownTimer = 0f;
+            isParrying = true;
+            parryCooldown = 2f;
+            StartCoroutine(DeactivateAction());
+        }
     }
     private IEnumerator DeactivateAction()
     {
