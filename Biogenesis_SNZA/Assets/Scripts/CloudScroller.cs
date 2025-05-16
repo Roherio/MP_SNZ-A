931f9dda7215c85280b0e41b2dc4cf3d07f0e715
Roger Garcia <rogergarciaviles@gmail.com>
2025-05-16 17:39:35 +0200

    Cambio de musica, nubes por capas, mas hitboxes fix

---
+++ Biogenesis_SNZA/Assets/Scripts/CloudScroller.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CloudScroller : MonoBehaviour
+{
+    private float speed = 0.5f;
+
+    public Transform OriginalPos;
+    public Transform FinalPos;
+
+  
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        if (transform.position.x > FinalPos.position.x)
+        {
+            transform.position = OriginalPos.position;
+        }
+    }
+}
