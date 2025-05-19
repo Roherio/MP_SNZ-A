46d4c9107bc7c70b938ac6a58d24e6849b7e72e0
Ferran Aliaga Rodriguez <aliagarf@campusemav.com>
2025-05-19 14:41:29 +0200

    ejecutable works

--- Biogenesis_SNZA/Assets/Scripts/Liora/Liora_Interaction_Script.cs
+++ Biogenesis_SNZA/Assets/Scripts/Liora/Liora_Interaction_Script.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
+//using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class Liora_Interaction_Script : MonoBehaviour
 {
--- Biogenesis_SNZA/Assets/Scripts/UIMenus/Menu_Script.cs
+++ Biogenesis_SNZA/Assets/Scripts/UIMenus/Menu_Script.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
+//using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class Menu_Script : MonoBehaviour
 {
