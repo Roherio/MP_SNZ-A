776e82df4650fb21aad01438c3cc39ac403d5abe
Roger Garcia <rogergarciaviles@gmail.com>
2025-05-25 18:33:14 +0200

    fix enemy sounds and hitbox

---
+++ Biogenesis_SNZA/Assets/Scripts/Music and FX/EnemySFXManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySFXManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (audioSource == null) audioSource = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        if (AudioSettingsManager.instance != null)
+        {
+            audioSource.volume = AudioSettingsManager.instance.effectsVol;
+        }
+    }
+}
